6f665e79feaee6000946f12c956a82a761dd8aa7
Nelzkie211 <neilandreireyes211@gmail.com>
2022-04-06 21:09:25 +0800

    corse = EmployeeAPI

--- NodeJS/employeeAPI/index.js
+++ NodeJS/employeeAPI/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const { mongoose } = require('./db.js')
 
 const EmployeeCntrl = require('./controllers/employeeController.js')
 
 const app = express()
 
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/employee', EmployeeCntrl)
 
